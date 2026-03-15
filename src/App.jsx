74df6582d9aff180920d21ba2957d21241b26755
Kumari Ayushi <ayushisomya1611@gmail.com>
2026-03-16 02:14:01 +0530

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import LoadingScreen from './components/LoadingScreen'
 import Navbar from './components/Navbar'
 import MobileMenu from './components/MobileMenu'
+import Home from './components/Home'
+import About from './components/About'
 import './index.css'
 function App() {
       const [isloading,setIsloaded]=useState(false)
@@ -13,6 +15,8 @@ function App() {
     <div className={`min-h-screen transition-opacity duration-700 ${isloading ?"opacity-100":"opacity-0"} bg-black text-gray-100`}>
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Home/>
+      <About/>
       </div> 
     </>
   )
